f6a2776b8844ed01066ed8a09a2834cc6da01ae6
Armando Fernandez <codefilez@gmail.com>
2025-03-18 20:55:20 -0700

    Bot class which handles the bot instance created with a message handler. RugCheck class created, which will pull the latest MOST VIEWED meme tokens on solana chain.

---
+++ Bot.cs
@@ -0,0 +1,91 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace SolMemeDiscordBot
+{
+
+    public class Bot
+    {
+        private readonly DiscordSocketClient _client;
+        private string DiscordToken = String.Empty;
+
+        public Bot()
+        {
+            var config = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .Build();
+
+            this.DiscordToken = config["Discord_Token"];
+
+            this._client = new DiscordSocketClient();
+            this._client.MessageReceived += MessageHandler;
+        }
+
+        public async Task Start()
+        {
+            await this._client.LoginAsync(Discord.TokenType.Bot, DiscordToken);
+            Console.WriteLine("Bot Successfully Logged In");
+            await this._client.StartAsync();
+            Console.WriteLine("Bot is running!");
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageHandler(SocketMessage message)
+        {
+            if (!message.Author.IsBot)
+            {
+                Console.WriteLine($"Received message: {message.Content ?? String.Empty}");
+                {
+                    if (message.Content.ToLower() == "yo")
+                    {
+                        string[] greetings = new string[] { "Hey", "Hello", "Hi", "Wassup", "Waddup", "Howdy", "Yo", "You called?" };
+                        Random random = new Random();
+                        int index = random.Next(greetings.Length);
+
+                        await ReplyAsync(message, $"{greetings[index]} {message.Author.GlobalName}!");
+                    }
+                    else if (message.Content.ToLower().Contains("get tokens"))
+                    {
+                        string response = "Here are the MOST viewed tokens at this very moment:\n";
+                        RugCheck rugCheck = new RugCheck();
+                        string mostViewedTokens = await rugCheck.GetMostViewedTokens();
+                        response += mostViewedTokens;
+                        await ReplyAsync(message, response);
+                    }
+                }
+
+            }
+        }
+
+        private async Task ReplyAsync(SocketMessage message, string response)
+        {
+            await message.Channel.SendMessageAsync(response);
+        }
+
+        // Process the message from the end-user and get a response, depending on the user's request.
+        private async Task<object> GetResponse(SocketMessage message)
+        {
+            object response = null;
+
+            try
+            {
+                response = "Cool";
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                response = "I'm sorry, I don't understand that command."; ;
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Author: Armando Fernandez (DevMando).
+ * Date: 03/18/2025
+ * Project: SoleMemeDiscordBot
+ * Description: A Discord bot to retrieve trending Solana meme tokens.
+ * 
+ */
+using Microsoft.Extensions.Configuration;
+using SolMemeDiscordBot;
+
+Bot bot = new Bot();
+await bot.Start();
